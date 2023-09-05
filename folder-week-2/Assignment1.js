c5f623a9c53bf55b1790dc07d29b70bb254cdd72
Crystal Chen <crystal.r.chen@gmail.com>
2023-09-05 14:41:48 +0800

    no message

---
+++ folder-week-2/Assignment1.js
@@ -0,0 +1,109 @@
+//Assignment 1: Function and Array
+//Complete the function below to find the max number of the passing array of numbers.
+//Reminder: you can't use built-in Math.max() or sort() to complete this assignment.
+
+/*function max(numbers) {
+const arr1 = [...numbers];
+ for (i=0; i< arr1.length; i++)
+
+if ( arr1[0]>=arr1[i]) {
+  console.log(arr1[0]);
+}
+else if ( arr1[1]>=arr1[i]){
+    console.log(arr1[1]);
+}
+else if(arr1[2]>=arr1[i]) {
+    console.log(arr1[2]);
+}
+else if(arr1[3]>=arr1[i]) {
+    console.log(arr1[3]);
+}
+else if(arr1[4]>=arr1[i]) {
+    console.log(arr1[4]);
+}
+else if(arr1[5]>=arr1[i]) {
+        console.log(arr1[5]);
+}else{ console.log('no amswer!')}
+
+}*/
+function max(numbers) {
+  const arr1 = [...numbers];
+  for (i = 0; i < arr1.length; i++) {
+    let NumMax = arr1[0];
+    if (arr1[i] > NumMax) {
+      NumMax = arr1[i];
+    }
+  }
+  console.log(NumMax);
+}
+console.log(max([1, 2, 4, 5])); // expected output: 5
+console.log(max([5, 2, 7, 1, 6])); // expected output: 7
+
+//Assignment 2: Function and Object
+//Complete the function below to calculate the result of the passing object.
+
+function calculate(args) {
+  const { n1, n2, op } = args;
+  let value;
+  if (op === "+") {
+    value = n1 + n2;
+  } else if (op === "-") {
+    value = n1 - n2;
+  } else {
+    value = "Not supported";
+  }
+  return value;
+}
+
+console.log(calculate({ n1: 2, n2: 3, op: "+" })); // expected output: 5
+console.log(calculate({ n1: 5, n2: 2, op: "-" })); // expected output: 3
+console.log(calculate({ n1: 1, n2: 6, op: "x" })); // expected output: 'Not supported'
+
+//Assignment 3: Function, Array, and Object
+//Complete the function below to calculate the total price of all products after applying a discount.
+
+function calculate(data) {
+  // 取出object 的price值,在把他們乘以優惠 就會得到答案
+  const { discount, products } = data; // 使用 reduce 函數將價格相加
+  const total = products.reduce((acc, product) => acc + product.price, 0);
+  const totalPrice = discount * total;
+
+  console.log(totalPrice);
+}
+const discountedPrice = calculate({
+  discount: 0.1,
+  products: [
+    {
+      name: "Product 1",
+      price: 100,
+    },
+    {
+      name: "Product 2",
+      price: 700,
+    },
+    {
+      name: "Product 3",
+      price: 250,
+    },
+  ],
+});
+
+// show the total price of all products after applying a discount
+
+第一週作業延伸;
+//Assignment 4: HTML DOM and Event Handling
+//Following assignment in week 1, let’s add some effects on it by only pure JavaScript
+//without Bootstrap, jQuery or any other libraries.
+//Request 1: Click to Change Text.
+//When the user clicks on the "Welcome Message" block, change text to "Have a Good Time!".
+
+//Request 2: Click to Show/Close Menu.
+//When the user clicks the menu at the top-right corner, show the hidden mobile menu.
+//After that, the user can click the close button to hide it.
+//Hint: you may apply css position:fixed to create a floating mobile menu,
+//which may be out of screen at the beginning and totally separated from the original menu.
+
+//Request 3: Click to Show More Content Boxes.
+//There are some more content boxes waiting to show. When the user clicks the Call-to-Action button,
+//show those hidden content boxes.
+//Hint: all content boxes are already there, they are just set to display:none at the beginning.
