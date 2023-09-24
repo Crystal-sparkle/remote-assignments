8d244cb1ee8493fb654cc30fdbd7a2f826f01658
Crystal Chen <crystal.r.chen@gmail.com>
2023-09-24 20:52:41 +0800

    class replace to className

--- folder-week-4/Assignment_3/src/App.js
+++ folder-week-4/Assignment_3/src/App.js
@@ -3,39 +3,39 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header class="main-header">
+      <header className="main-header">
         <div id="header">
-          <div class="topnav">
-            <nav class="toolbar">
-              <ul class="flex-nav">
+          <div className="topnav">
+            <nav className="toolbar">
+              <ul className="flex-nav">
                 <li>
                   <li>
-                    <a class="logo">Website Title / Logo</a>
+                    <a className="logo">Website Title / Logo</a>
                   </li>
-                  <div class="hamburgerbtn">
-                    <button class="hamburger"></button>
+                  <div className="hamburgerbtn">
+                    <button className="hamburger"></button>
                   </div>
                 </li>
 
-                <div class="items">
+                <div className="items">
                   <li>
-                    <a class="item item-1">item 1</a>
+                    <a className="item item-1">item 1</a>
                   </li>
                   <li>
-                    <a class="item item-2">item 2</a>
+                    <a className="item item-2">item 2</a>
                   </li>
                   <li>
-                    <a class="item item-3">item 3</a>
+                    <a className="item item-3">item 3</a>
                   </li>
                   <li>
-                    <a class="item item-4">item 4</a>
+                    <a className="item item-4">item 4</a>
                   </li>
                 </div>
               </ul>
             </nav>
           </div>
         </div>
-        <nav id="mobile-nav" class="mobile-nav">
+        <nav id="mobile-nav" className="mobile-nav">
           <a href="#">item1</a>
           <a href="#">item2</a>
           <a href="#">item3</a>
@@ -47,21 +47,21 @@ function App() {
 
         <h3>Section Title</h3>
 
-        <div class="contentbox">
-          <div class="box box-1">Content Box1</div>
-          <div class="box box-2">Content Box2</div>
-          <div class="box box-3">Content Box3</div>
-          <div class="box box-4">Content Box4</div>
+        <div className="contentbox">
+          <div className="box box-1">Content Box1</div>
+          <div className="box box-2">Content Box2</div>
+          <div className="box box-3">Content Box3</div>
+          <div className="box box-4">Content Box4</div>
         </div>
       </section>
-      <div class="firstBtn">
+      <div className="firstBtn">
         <button id="calltoAction">Call to Action</button>
       </div>
-      <div id="contentbox2" class="contentbox">
-        <div class="box box2 box-1">Content Box5</div>
-        <div class="box box2 box-2">Content Box6</div>
-        <div class="box box2 box-3">Content Box7</div>
-        <div class="box box2 box-4">Content Box8</div>
+      <div id="contentbox2" className="contentbox">
+        <div className="box box2 box-1">Content Box5</div>
+        <div className="box box2 box-2">Content Box6</div>
+        <div className="box box2 box-3">Content Box7</div>
+        <div className="box box2 box-4">Content Box8</div>
       </div>
     </div>
   );
